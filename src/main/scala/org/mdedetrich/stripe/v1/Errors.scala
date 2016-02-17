3439fe4118890094f7c65cc2ad824b67bde51459
Matthew de Detrich <mdedetrich@gmail.com>
2016-02-17 15:06:36 +1100

    Added in the Code type in Errors

--- src/main/scala/org/mdedetrich/stripe/v1/Errors.scala
+++ src/main/scala/org/mdedetrich/stripe/v1/Errors.scala
@@ -5,6 +5,10 @@ import play.api.libs.json.{JsString, Writes, Reads}
 
 object Errors {
 
+  /**
+    * Errors taken from https://stripe.com/docs/api#errors
+    * @param id
+    */
   sealed abstract class Error(val id: String)
 
   case class UnknownError(val id: String) extends Exception {
@@ -38,6 +42,51 @@ object Errors {
   implicit val errorWrites: Writes[Error] =
     Writes((error: Error) => JsString(error.id))
 
+  /**
+    * Codes taken from https://stripe.com/docs/api#errors
+    * @param id
+    */
+  sealed abstract class Code(val id: String)
+
+  case class UnknownCode(val id: String) extends Exception {
+    override def getMessage = "Unknown Error code, received $id"
+  }
+  
+  object Code {
+    case object InvalidNumber extends Code("invalid_number")
+    
+    case object InvalidExpiryMonth extends Code("invalid_expiry_month")
+    
+    case object InvalidExpiryYear extends Code("invalid_expiry_year")
+    
+    case object InvalidCVC extends Code("invalid_cvc")
+    
+    case object IncorrectNumber extends Code("incorrect_number")
+    
+    case object ExpiredCard extends Code("expired_card")
+    
+    case object IncorrectCVC extends Code("incorrect_cvc")
+    
+    case object IncorrectZip extends Code("incorrect_zip")
+    
+    case object CardDeclined extends Code("card_declined")
+    
+    case object Missing extends Code("missing")
+    
+    case object ProcessingError extends Code("processing_error")
+    
+    lazy val all: Set[Code] = SealedContents.values[Code]
+  }
+
+  implicit val codeReads: Reads[Code] = Reads.of[String].map { codeId =>
+    Code.all.find(_.id == codeId).getOrElse {
+      throw UnknownCode(codeId)
+    }
+  }
+  
+  implicit val codeWrites: Writes[Code] =
+    Writes((code: Code) => JsString(code.id))
+  
 }
 
 
