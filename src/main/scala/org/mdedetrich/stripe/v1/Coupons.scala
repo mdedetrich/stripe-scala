fe1765e6a892e6e00be933843f0e965848a2a410
Matthew de Detrich <mdedetrich@gmail.com>
2016-02-21 11:37:13 +1100

    Added endpoint for coupon

--- src/main/scala/org/mdedetrich/stripe/v1/Coupons.scala
+++ src/main/scala/org/mdedetrich/stripe/v1/Coupons.scala
@@ -1,12 +1,19 @@
 package org.mdedetrich.stripe.v1
 
+import com.typesafe.scalalogging.LazyLogging
+import org.mdedetrich.stripe.{InvalidJsonModelException, Endpoint, ApiKey, IdempotencyKey}
+import dispatch.Defaults._
+import dispatch._
 import org.mdedetrich.utforsca.SealedContents
 import org.joda.time.DateTime
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import org.mdedetrich.playjson.Utils._
 
-object Coupons {
+import scala.concurrent.Future
+import scala.util.Try
+
+object Coupons extends LazyLogging {
 
   sealed abstract class Duration(val id: String)
 
@@ -90,4 +97,98 @@ object Coupons {
       )
     )
 
+  case class CouponInput(id: Option[String],
+                         duration: Duration,
+                         amountOff: Option[Long],
+                         currency: Option[Currency],
+                         durationInMonths: Option[Long],
+                         maxRedemptions: Option[Long],
+                         metadata: Option[Map[String, String]],
+                         percentOff: Option[BigDecimal],
+                         redeemBy: Option[DateTime]
+                        )
+  
+  implicit val couponInputReads: Reads[CouponInput] = (
+    (__ \ "id").readNullable[String] ~
+    (__ \ "duration").read[Duration] ~
+    (__ \ "amount_off").readNullable[Long] ~
+    (__ \ "currency").readNullable[Currency] ~
+    (__ \ "duration_in_months").readNullable[Long] ~
+    (__ \ "max_redemptions").readNullable[Long] ~
+    (__ \ "metadata").readNullableOrEmptyJsObject[Map[String,String]] ~
+    (__ \ "percent_off").readNullable[BigDecimal] ~
+    (__ \ "redeemBy").readNullable[Long].map(_.map{timestamp => new DateTime(timestamp * 1000)})
+    ).tupled.map(CouponInput.tupled)
+  
+  implicit val couponInputWrites: Writes[CouponInput] =
+    Writes((couponInput: CouponInput) =>
+      Json.obj(
+        "id" -> couponInput.id,
+        "duration" -> couponInput.duration,
+        "amount_off" -> couponInput.amountOff,
+        "currency" -> couponInput.currency,
+        "duration_in_months" -> couponInput.durationInMonths,
+        "max_redemptions" -> couponInput.maxRedemptions,
+        "metadata" -> couponInput.metadata,
+        "percent_off" -> couponInput.percentOff,
+        "redeemBy" -> couponInput.redeemBy.map(_.getMillis / 1000)
+      )
+    )
+  
+  def create(couponInput: CouponInput,
+             idempotencyKey: Option[IdempotencyKey] = None
+            )
+            (implicit apiKey: ApiKey,
+             endpoint: Endpoint): Future[Try[Coupon]] = {
+    val postFormParameters: Map[String,String] = {
+      Map(
+        "id" -> couponInput.id,
+        "duration" -> Option(couponInput.duration.id),
+        "amount_off" -> couponInput.amountOff.map(_.toString),
+        "currency" -> couponInput.currency.map(_.iso.toLowerCase),
+        "duration_in_months" -> couponInput.durationInMonths.map(_.toString),
+        "max_redemptions" -> couponInput.maxRedemptions.map(_.toString),
+        "percent_off" -> couponInput.percentOff.map(_.toString()),
+        "redeemBy" -> couponInput.redeemBy.map(dateTime => (dateTime.getMillis / 1000).toString)
+      ).collect {
+        case (k, Some(v)) => (k, v)
+      }
+    } ++ mapToPostParams(couponInput.metadata, "metadata")
+
+    logger.debug(s"Generated POST form parameters is $postFormParameters")
+
+    val finalUrl = endpoint.url + "/v1/charges"
+
+    val req = {
+      val r = (
+        url(finalUrl)
+          .addHeader("Content-Type", "application/x-www-form-urlencoded")
+          << postFormParameters
+        ).POST.as(apiKey.apiKey, "")
+
+      idempotencyKey match {
+        case Some(key) =>
+          r.addHeader("Idempotency-Key", key.key)
+        case None =>
+          r
+      }
+    }
+
+    Http(req).map { response =>
+
+      parseStripeServerError(response, finalUrl, Option(postFormParameters), None)(logger) match {
+        case Right(triedJsValue) =>
+          triedJsValue.map { jsValue =>
+            val jsResult = Json.fromJson[Coupon](jsValue)
+            jsResult.fold(
+              errors => {
+                throw InvalidJsonModelException(response.getStatusCode, finalUrl, Option(postFormParameters), None, jsValue, errors)
+              }, coupon => coupon
+            )
+          }
+        case Left(error) =>
+          scala.util.Failure(error)
+      }
+    }
+  }
 }
