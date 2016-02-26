ea072cc55a212565c8ae24f96db60e2ee354866d
Matthew de Detrich <mdedetrich@gmail.com>
2016-02-26 15:22:12 +1100

    Removed unneeded AnyVal, not being used

--- src/main/scala/org/mdedetrich/stripe/MaxNumberOfRetries.scala
+++ src/main/scala/org/mdedetrich/stripe/MaxNumberOfRetries.scala
@@ -1,7 +1,5 @@
 package org.mdedetrich.stripe
 
-case class NumberOfRetries(numberOfRetries: Long) extends AnyVal
-
 case class MaxNumberOfRetries(val numberOfRetries: Int) extends Exception {
   override def getMessage = s"Wen't over max number of retries, retries is $numberOfRetries"
 }
