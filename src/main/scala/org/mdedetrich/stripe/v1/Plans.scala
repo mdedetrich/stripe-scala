98627c2f8ddf17220b6ccdd1f1f08e99e4a50f6e
Matthew de Detrich <mdedetrich@gmail.com>
2016-03-01 17:18:38 +1100

    Added endpoints for Plan

--- src/main/scala/org/mdedetrich/stripe/v1/Plans.scala
+++ src/main/scala/org/mdedetrich/stripe/v1/Plans.scala
@@ -1,12 +1,19 @@
 package org.mdedetrich.stripe.v1
 
+import com.typesafe.scalalogging.LazyLogging
 import enumeratum._
 import org.joda.time.DateTime
+import org.mdedetrich.stripe.{InvalidJsonModelException, Endpoint, ApiKey, IdempotencyKey}
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import org.mdedetrich.playjson.Utils._
+import dispatch.Defaults._
+import dispatch._
 
-object Plans {
+import scala.concurrent.Future
+import scala.util.Try
+
+object Plans extends LazyLogging {
 
   sealed abstract class Interval(val id: String) extends EnumEntry {
     override val entryName = id
@@ -67,6 +74,33 @@ object Plans {
                   trialStart: Option[DateTime]
                  )
 
+  object Plan {
+    def default(id: String,
+                amount: BigDecimal,
+                created: DateTime,
+                currency: Currency,
+                interval: Interval,
+                intervalCount: Long,
+                livemode: Boolean,
+                name: String): Plan = Plan(
+      id,
+      amount,
+      created,
+      currency,
+      interval,
+      intervalCount,
+      livemode,
+      None,
+      name,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None
+    )
+  }
+
   implicit val planReads: Reads[Plan] = (
     (__ \ "id").read[String] ~
       (__ \ "amount").read[BigDecimal] ~
@@ -104,4 +138,117 @@ object Plans {
       "trial_end" -> plan.trialEnd.map(x => Json.toJson(x)(stripeDateTimeWrites)),
       "trial_start" -> plan.trialStart.map(x => Json.toJson(x)(stripeDateTimeWrites))
     ))
+
+  case class PlanInput(id: String,
+                       amount: BigDecimal,
+                       currency: Currency,
+                       interval: Interval,
+                       name: String,
+                       intervalCount: Option[Long],
+                       metadata: Option[Map[String, String]],
+                       statementDescriptor: Option[String],
+                       trialPeriodDays: Option[Long]
+                      )
+
+  object PlanInput {
+    def default(id: String,
+                amount: BigDecimal,
+                currency: Currency,
+                interval: Interval,
+                name: String): PlanInput = PlanInput(
+      id,
+      amount,
+      currency,
+      interval,
+      name,
+      None,
+      None,
+      None,
+      None
+    )
+  }
+
+  implicit val planInputReads: Reads[PlanInput] = (
+    (__ \ "id").read[String] ~
+      (__ \ "amount").read[BigDecimal] ~
+      (__ \ "currency").read[Currency] ~
+      (__ \ "interval").read[Interval] ~
+      (__ \ "name").read[String] ~
+      (__ \ "interval_count").readNullable[Long] ~
+      (__ \ "metadata").readNullableOrEmptyJsObject[Map[String, String]] ~
+      (__ \ "statement_descriptor").readNullable[String] ~
+      (__ \ "trial_period_days").readNullable[Long]
+    ).tupled.map((PlanInput.apply _).tupled)
+  
+  implicit val planInputWrites: Writes[PlanInput] =
+    Writes ((planInput: PlanInput) =>
+      Json.obj(
+        "id" -> planInput.id,
+        "amount" -> planInput.amount,
+        "currency" -> planInput.currency,
+        "interval" -> planInput.interval,
+        "name" -> planInput.name,
+        "interval_count" -> planInput.intervalCount,
+        "metadata" -> planInput.metadata,
+        "statement_descriptor" -> planInput.statementDescriptor,
+        "trial_period_days" -> planInput.trialPeriodDays
+      )
+    )
+
+  def create(planInput: PlanInput)
+            (idempotencyKey: Option[IdempotencyKey] = None)
+            (implicit apiKey: ApiKey,
+             endpoint: Endpoint): Future[Try[Plan]] = {
+    val postFormParameters: Map[String, String] = {
+      Map(
+        "id" -> Option(planInput.id.toString),
+        "amount" -> Option(planInput.amount.toString()),
+        "currency" -> Option(planInput.currency.iso.toLowerCase),
+        "interval" -> Option(planInput.interval.id.toString),
+        "name" -> Option(planInput.name),
+        "interval_count" -> planInput.intervalCount.map(_.toString),
+        "statement_descriptor" -> planInput.statementDescriptor,
+        "trial_period_days" -> planInput.trialPeriodDays.map(_.toString)
+      ).collect {
+        case (k, Some(v)) => (k, v)
+      }
+    } ++ mapToPostParams(planInput.metadata, "metadata")
+
+    logger.debug(s"Generated POST form parameters is $postFormParameters")
+
+    val finalUrl = endpoint.url + "/v1/plans"
+
+    val req = {
+      val r = (
+        url(finalUrl)
+          .addHeader("Content-Type", "application/x-www-form-urlencoded")
+          << postFormParameters
+        ).POST.as(apiKey.apiKey, "")
+
+      idempotencyKey match {
+        case Some(key) =>
+          r.addHeader(idempotencyKeyHeader, key.key)
+        case None =>
+          r
+      }
+    }
+
+    Http(req).map { response =>
+
+      parseStripeServerError(response, finalUrl, Option(postFormParameters), None)(logger) match {
+        case Right(triedJsValue) =>
+          triedJsValue.map { jsValue =>
+            val jsResult = Json.fromJson[Plan](jsValue)
+            jsResult.fold(
+              errors => {
+                throw InvalidJsonModelException(response.getStatusCode, finalUrl, Option(postFormParameters), None, jsValue, errors)
+              }, plan => plan
+            )
+          }
+        case Left(error) =>
+          scala.util.Failure(error)
+      }
+    }
+  }
+
 }
