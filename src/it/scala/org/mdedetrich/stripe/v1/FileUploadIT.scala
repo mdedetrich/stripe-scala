fcce0e312d0d2be583d48cccbc7aa58612d648a5
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2016-10-07 17:55:06 +0200

    Add result entity for file uploads

--- src/it/scala/org/mdedetrich/stripe/v1/FileUploadIT.scala
+++ src/it/scala/org/mdedetrich/stripe/v1/FileUploadIT.scala
@@ -2,13 +2,13 @@ package org.mdedetrich.stripe.v1
 
 import java.time.OffsetDateTime
 
-import org.mdedetrich.stripe.v1.FileUploads.Purpose
+import org.mdedetrich.stripe.v1.FileUploads.{FileUpload, Purpose}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{Matchers, WordSpec}
 import play.api.libs.json.JsValue
 
-class FileUploadSpec extends WordSpec with Matchers with ScalaFutures {
+class FileUploadIT extends WordSpec with Matchers with ScalaFutures {
 
   implicit val defaultPatience = PatienceConfig(timeout = Span(10, Seconds), interval = Span(5, Millis))
 
@@ -22,7 +22,7 @@ class FileUploadSpec extends WordSpec with Matchers with ScalaFutures {
       val uploadF = handle(FileUploads.upload(Purpose.IdentityDocument, s"file-upload-${OffsetDateTime.now}.jpg", is))
 
       whenReady(uploadF) { u =>
-        u shouldBe a[JsValue]
+        u shouldBe a[FileUpload]
       }
     }
   }
--- src/main/scala/org/mdedetrich/stripe/v1/FileUploads.scala
+++ src/main/scala/org/mdedetrich/stripe/v1/FileUploads.scala
@@ -1,22 +1,27 @@
 package org.mdedetrich.stripe.v1
 
 import java.io.InputStream
+import java.time.OffsetDateTime
 
 import com.ning.http.client.multipart.{ByteArrayPart, StringPart}
 import com.typesafe.scalalogging.LazyLogging
 import dispatch.{Http, url}
-import enumeratum.{Enum, EnumEntry, EnumFormats}
+import enumeratum.{Enum, EnumEntry, EnumFormats, PlayJsonEnum}
 import org.mdedetrich.stripe.{ApiKey, FileUploadEndpoint, InvalidJsonModelException}
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
 object FileUploads extends LazyLogging {
 
-  sealed abstract class Purpose(val value: String) extends EnumEntry
+  // Purpose
+  sealed abstract class Purpose(value: String) extends EnumEntry {
+    override val entryName = value
+  }
 
-  object Purpose extends Enum[Purpose] {
+  object Purpose extends Enum[Purpose] with PlayJsonEnum[Purpose] {
     val values = findValues
 
     case object BusinessLogo extends Purpose("business_logo")
@@ -30,10 +35,30 @@ object FileUploads extends LazyLogging {
     implicit val purposeFormats = EnumFormats.formats(Purpose)
   }
 
+  // FileUpload
+
+  case class FileUpload(
+    id: String,
+    created: OffsetDateTime,
+    purpose: Purpose,
+    size: Long,
+    url: Option[String]
+  )
+
+  implicit val customerReads: Reads[FileUpload] = (
+      (__ \ "id").read[String] ~
+      (__ \ "created").read[OffsetDateTime](stripeDateTimeReads) ~
+      (__ \ "purpose").read[Purpose] ~
+      (__ \ "size").read[Long] ~
+      (__ \ "url").readNullable[String]
+    ).tupled.map((FileUpload.apply _).tupled)
+
+  implicit val fileUploadReadsWrites = Json.writes[FileUpload]
+
   def upload(purpose: Purpose, fileName: String, inputStream: InputStream)(
       implicit apiKey: ApiKey,
       endpoint: FileUploadEndpoint,
-      ec: ExecutionContext): Future[Try[JsValue]] = {
+      ec: ExecutionContext): Future[Try[FileUpload]] = {
 
     val data = Stream.continually(inputStream.read).takeWhile(_ != -1).map(_.toByte).toArray
     upload(purpose, fileName, data)
@@ -42,11 +67,11 @@ object FileUploads extends LazyLogging {
   def upload(purpose: Purpose, fileName: String, data: Array[Byte])(
       implicit apiKey: ApiKey,
       endpoint: FileUploadEndpoint,
-      ec: ExecutionContext): Future[Try[JsValue]] = {
+      ec: ExecutionContext): Future[Try[FileUpload]] = {
 
     val finalUrl = endpoint.url + s"/v1/files"
 
-    val paramsPart = new StringPart("purpose", purpose.value)
+    val paramsPart = new StringPart("purpose", purpose.entryName)
     val filePart = new ByteArrayPart("file", data, null, null, fileName)
 
     val req = url(finalUrl).addHeader("Content-Type", "multipart/form-data")
@@ -59,7 +84,7 @@ object FileUploads extends LazyLogging {
           response, finalUrl, None, None)(logger) match {
         case Right(triedJsValue) =>
           triedJsValue.map { jsValue =>
-            val jsResult = Json.fromJson[JsValue](jsValue)
+            val jsResult = Json.fromJson[FileUpload](jsValue)
             jsResult.fold(
                 errors => {
                   throw InvalidJsonModelException(response.getStatusCode,
