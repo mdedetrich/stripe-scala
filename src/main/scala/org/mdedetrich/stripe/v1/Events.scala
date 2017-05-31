89bbaebcc88c02ddd570583c815c35fbfc6a5449
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2017-05-31 16:55:14 +0200

    Add json test file

--- src/main/scala/org/mdedetrich/stripe/v1/Events.scala
+++ src/main/scala/org/mdedetrich/stripe/v1/Events.scala
@@ -99,7 +99,8 @@ object Events extends LazyLogging {
   case class Data(`object`: StripeObject, previousAttributes: Option[JsonObject])
 
   implicit val eventDataDecoder: Decoder[Data] = Decoder.forProduct2("object", "previous_attributes")(Data.apply)
-  implicit val eventDataEncoder: Encoder[Data] = Encoder.forProduct2("object", "previous_attributes")(x => Data.unapply(x).get)
+  implicit val eventDataEncoder: Encoder[Data] =
+    Encoder.forProduct2("object", "previous_attributes")(x => Data.unapply(x).get)
 
   case class Event(
       id: String,
